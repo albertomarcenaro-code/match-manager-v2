b14b79dd42632df4e558a0eb90e58268f2bd935c
albertomarcenaro-code <albertomarcenaro@gmail.com>
2026-01-23 08:37:30 +0100

    Update vite.config.ts
    automatic release

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,5 +15,10 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  // Questa parte definisce le variabili automatiche
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(pkg.version),
+    'import.meta.env.VITE_APP_BUILD_DATE': JSON.stringify(new Date().toLocaleDateString('it-IT')),
   },
 }));
